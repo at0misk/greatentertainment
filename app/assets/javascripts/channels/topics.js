844d2e3c7de5ec38a203377c139b061738417e17
at0misk <ktp925@gmail.com>
2017-06-12 10:08:12 -0700

    time zone

--- app/assets/javascripts/channels/topics.js
+++ app/assets/javascripts/channels/topics.js
@@ -1,10 +1,16 @@
 App.topics = App.cable.subscriptions.create('TopicsChannel', {  
   received: function(data) {
   	$("#req_table" + data.user_id).removeClass('hide');
+  	$(".topicsList" + data.user_id).append(this.renderTopicList(data))
+  	$(".topicsListEmpty" + data.user_id).addClass('hide')
     return $(".topics" + data.user_id).append(this.renderTopic(data));
   },
 
   renderTopic: function(data) {
+    return  "<tr><td><a href='/topics/" + data.id + "'>" + data.name + '</a></td><td>' + data.request_name + '</td><td>' + data.time + '</td></tr>';
+  }
+
+  renderTopicList: function(data) {
     return  "<tr><td><a href='/topics/" + data.id + "'>" + data.name + '</a></td><td>' + data.request_name + '</td><td>' + data.time + '</td></tr>';
   }
 
