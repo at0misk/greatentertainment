76dce9018200bb4080413c3a8ee479ed859d5c26
at0misk <ktp925@gmail.com>
2017-06-23 11:22:14 -0700

    fe

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
 	def new
+		if session[:page_user_id]
+			@page_user = User.find(session[:page_user_id])
+			redirect_to "/#{@page_user.username}" and return
+		end
 	end
 	def create
 		@user = User.find_by(email: params['email'])
