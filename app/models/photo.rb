ba72645461b95ef5437f95dcf511b350f7e699f7
at0misk <ktp925@gmail.com>
2017-07-17 16:12:35 -0700

    fe

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,5 @@ class Photo < ApplicationRecord
   do_not_validate_attachment_file_type :image
   belongs_to :user
   has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
-  validates_with AttachmentSizeValidator, attributes: :image, less_than: 2.megabytes
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
 end
