10541f9634281a768eaf064c2d9b354a6135ad70
at0misk <ktp925@gmail.com>
2017-07-17 14:53:15 -0700

    blogfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.time_zone = "Pacific Time (US & Canada)"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -50,6 +50,17 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "evolution_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => "archertravelmailer",
+   :password             => "billions1!",
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
