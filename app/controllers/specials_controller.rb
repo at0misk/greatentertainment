ffb0baeb29a5809f467748c0c10cebe45dda2223
at0misk <ktp925@gmail.com>
2017-07-24 15:08:37 -0700

    special errors

--- app/controllers/specials_controller.rb
+++ app/controllers/specials_controller.rb
@@ -79,42 +79,47 @@ class SpecialsController < ApplicationController
 		@page_user = User.find(session[:page_user_id]) if session[:page_user_id]
 		if @@fj.present?
 			@fj = @@fj
-			desc_doc = Nokogiri::HTML(open("#{@fj.ref}"))
-			if @fj.location == "Hawaii"
-				# @fj.img_src = desc_doc.css('.jumbo-hero>.selected')[0]['src']
-				# @fj.img_src.slice!(0)
-				# @fj.img_src = "https://www.expedia.com" + @fj.img_src
-				# puts @fj.img_src
-				# fail
-				# @description = desc_doc.css('#ABOUT_TAB')
-				@facilities = desc_doc.css('.hotel-description').to_s
-				@facilities.strip
-				puts @facilities
-				# fail
-				# @facilities = @facilities.gsub(/.*?(?=HOTEL FACILITIES)/im, "")
-				# puts @facilities
-				# fail
-				# puts @description.gsub("\t", "")
-				# @second_picture = desc_doc.css('.vfmPhoto')[0]
-				# puts @second_picture
-				# fail
-			elsif @fj.location == "Mexico"
-				@new_src = desc_doc.css('#gallery_0>img')[0]['src']
-				@fj.img_src = "http://www.cheapcaribbean.com" + @new_src
-				@description = desc_doc.css(".mobileResortLove .padded").text
-				@why = desc_doc.css('.mobileResortGo .padded').text
-				# puts @description
-				# fail
-			elsif @fj.location == "Cruise"
-				@description = desc_doc.css('.intro-text').text.gsub("â", "'")
-				@map = "https://www.ncl.com" + desc_doc.css('.intro-figure>img')[0]['src']
-				@table = desc_doc.css('.table td')
-				# rowss = data.css("td[valign='top'] table tr") # All the <tr>this is a line</tr>
-				@table.each do |row|
-				  puts row.text # Will print all the 'this is a line'
+			if @fj.ref != ''
+				desc_doc = Nokogiri::HTML(open("#{@fj.ref}"))
+				if @fj.location == "Hawaii"
+					# @fj.img_src = desc_doc.css('.jumbo-hero>.selected')[0]['src']
+					# @fj.img_src.slice!(0)
+					# @fj.img_src = "https://www.expedia.com" + @fj.img_src
+					# puts @fj.img_src
+					# fail
+					# @description = desc_doc.css('#ABOUT_TAB')
+					@facilities = desc_doc.css('.hotel-description').to_s
+					@facilities.strip
+					puts @facilities
+					# fail
+					# @facilities = @facilities.gsub(/.*?(?=HOTEL FACILITIES)/im, "")
+					# puts @facilities
+					# fail
+					# puts @description.gsub("\t", "")
+					# @second_picture = desc_doc.css('.vfmPhoto')[0]
+					# puts @second_picture
+					# fail
+				elsif @fj.location == "Mexico"
+					@new_src = desc_doc.css('#gallery_0>img')[0]['src']
+					@fj.img_src = "http://www.cheapcaribbean.com" + @new_src
+					@description = desc_doc.css(".mobileResortLove .padded").text
+					@why = desc_doc.css('.mobileResortGo .padded').text
+					# puts @description
+					# fail
+				elsif @fj.location == "Cruise"
+					@description = desc_doc.css('.intro-text').text.gsub("â", "'")
+					@map = "https://www.ncl.com" + desc_doc.css('.intro-figure>img')[0]['src']
+					@table = desc_doc.css('.table td')
+					# rowss = data.css("td[valign='top'] table tr") # All the <tr>this is a line</tr>
+					@table.each do |row|
+					  puts row.text # Will print all the 'this is a line'
+					end
+				else
+					@description = desc_doc.css('#overviewHotelProperty').text
 				end
 			else
-				@description = desc_doc.css('#overviewHotelProperty').text
+				flash[:errors] = "Special not available."
+				redirect_to "/#{@page_user.username}"
 			end
 			# fail
 		else
