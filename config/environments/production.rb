fdae06893e3cdb1b31d67d971d4270bc2c26eb90
at0misk <ktp925@gmail.com>
2017-07-31 09:59:27 -0700

    beta test emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
   :enable_starttls_auto => true
   }
   config.action_mailer.perform_caching = false
-  config.action_mailer.asset_host = 'https://52.10.80.38'
-  config.action_controller.asset_host = 'https://52.10.80.38'
+  config.action_mailer.asset_host = 'https://evotravelagent.com'
+  config.action_controller.asset_host = 'https://evotravelagent.com'
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
