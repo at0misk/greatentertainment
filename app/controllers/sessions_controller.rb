35931af69b74077a772bed04643435ae289b49bc
at0misk <ktp925@gmail.com>
2017-08-01 16:22:03 -0700

    fe

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		session[:page_user_id] = nil
 	end
 	def create
-		@user = User.find_by(email: params['email'].downcase)
+		@user = User.find_by(email: params['email'])
 		if @user
 			if @user.authenticate(params['password'])
 				session[:user_id] = @user.id
---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+Devise.setup do |config|
+  config.case_insensitive_keys = [:email, :username]
+end
