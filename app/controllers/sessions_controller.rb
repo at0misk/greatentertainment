d0adfe2a0775a37fb9f71b99cf57fabfc00f028a
at0misk <ktp925@gmail.com>
2017-08-01 16:31:19 -0700

    fe

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		session[:page_user_id] = nil
 	end
 	def create
-		@user = User.find_by(User.where("LOWER(email) = ?", params['email'].downcase))
+		@user = User.find_by("LOWER(email) = ?", params['email'].downcase)
 		if @user
 			if @user.authenticate(params['password'])
 				session[:user_id] = @user.id
